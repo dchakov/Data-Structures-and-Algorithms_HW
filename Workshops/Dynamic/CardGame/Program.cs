18862f4893f9fdde5c03053556ba6bcb139308e9
dchakov <dchakov@yahoo.com>
2015-11-25 13:04:10 +0200

    graph

---
+++ Workshops/Dynamic/CardGame/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CardGame
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = Console.ReadLine().Split(' ')
+                .Select(a => int.Parse(a)).ToArray();
+
+            int[,] arr = new int[n, n];
+
+            for (int lenght = 3; lenght <= n; lenght++)
+            {
+                for (int i = 0; i <= n - lenght; i++)
+                {
+                    for (int j = i + 1; j < i + lenght - 1; j++)
+                    {
+                        int current = arr[i, j] + arr[j, i + lenght - 1]
+                             + numbers[j] * (numbers[i] + numbers[i + lenght - 1]);
+
+                        if (arr[i, i + lenght - 1] < current)
+                        {
+                            arr[i, i + lenght - 1] = current;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(arr[0, n - 1]);
+        }
+    }
+}
---
+++ Workshops/Dynamic/FiboDynamic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiboDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 40;
+            int[] fibo = new int[100];
+
+            fibo[0] = 1;
+            fibo[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+            }
+            Console.WriteLine(fibo[n]);
+        }
+    }
+}
